4ae1e7ae37a56397b2654dd3ea4753b2f1e940f3
madhu <madhubabu@gmail.com>
2025-04-02 13:55:58 +0530

    craterootpackage.json

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,25 @@
 import express from "express";
-import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import cors from "cors";
-import authRouter from "./routes/authRoutes.js";
 
-// Load environment variables
-dotenv.config();
-
-const port = process.env.PORT || 2000;
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-// Backend API Routes
-app.use("/auth", authRouter);
-
-// Serve Frontend Static Files
+// Serve React frontend
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const frontendPath = path.join(__dirname, "../frontend/dist");
-
-app.use(express.static(frontendPath));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
-// Start Server
-app.listen(port, () => {
-  console.log(`✅ Server is Running on port ${port}`);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
