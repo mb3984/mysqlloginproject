13c99de9d752b069d690eb29227ba1f90fb03a7a
madhu <madhubabu@gmail.com>
2025-04-02 14:35:44 +0530

    changepackage.json

--- backend/server.js
+++ backend/server.js
@@ -6,19 +6,25 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 2000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // Serve React frontend
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-});
-
+if (process.env.NODE_ENV === "production") {
+  const staticPath = path.resolve(__dirname, "../frontend/dist");
+  app.use(express.static(staticPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(staticPath, "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Development Mode: Server is running.");
+  });
+}
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
